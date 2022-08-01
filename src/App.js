9cd4a27be091276050909b67a26cd3c34b356889
DOMAYA <c2hila@gmail.com>
2022-08-01 22:05:49 +0900

    Update style

--- src/App.js
+++ src/App.js
@@ -7,14 +7,15 @@ export default class app extends Component {
       {
         id: "1",
         title: "공부하기",
-        completed: true
+        completed: false
       },
       {
         id:"2",
         title:"청소하기",
         completed: false
       },
-    ]
+    ],
+    value : ""
   }
 
   btnStyle = {
@@ -26,11 +27,11 @@ export default class app extends Component {
     float: "right"
   }
 
-  getStyle = () => {
+  getStyle = (completed) => {
     return {
       padding: "10px",
       borderBottom: "1px #ccc dotted",
-      textDecoration: "none"
+      textDecoration: completed ? "line-through" : "none"
     }
   }
 
@@ -38,6 +39,22 @@ export default class app extends Component {
   handleClick = (id) => {
     let newTodoData = this.state.todoData.filter((data => data.id !== id))
     this.setState({todoData : newTodoData});
+  }
+
+  handleChange = (e) => {
+    this.setState({ value : e.target.value })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    let newTodo = {
+      id : Date.now(),
+      title : this.state.value,
+      completed : false,
+    }
+
+    this.setState( { todoData : [...this.state.todoData, newTodo]} )
   }
   
   render() {    
@@ -47,18 +64,23 @@ export default class app extends Component {
             <div className="title">
               <h1>할 일 목록</h1>
             </div>
-            <form style={{display : 'flex' }}>
+            <form style={{display : 'flex' }} onSubmit = {this.handleSubmit}>
               <input
                   type="text"
                   name="value"
                   sytle={{ flex: '10', padding : '5px' }}
                   placeholder = "해야 할 일을 입력하세요"
-                  value=""/>
-
+                  value={this.state.value}
+                  onChange = { this.handleChange}
+                  />
+              <input 
+                  type = "submit"
+                  value = "입력"
+              />
             </form>
 
               {this.state.todoData.map((data)=>(
-                <div style={this.getStyle()} key={data.id}>
+                <div style={this.getStyle(data.completed)} key={data.id}>
                     <input type="checkbox" defaultChecked={false} />
                       {data.title}
                     <button style={this.btnStyle} onClick={()=> this.handleClick(data.id)}>x</button>
