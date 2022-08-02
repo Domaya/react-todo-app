5edb58ca6d6f07220030506b9484b0536f64b0df
DOMAYA <c2hila@gmail.com>
2022-08-02 16:42:39 +0900

    tailWinds CSS로 스타일링

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,20 +6,19 @@ export default function Form (props) {
       }
     
   return (
-        <form style={{display : 'flex' }} onSubmit = {props.handleSubmit}>
+        <form className='flex pt-2' onSubmit = {props.handleSubmit}>
                 <input
+                    className="w-full px-3 py-2 mr-4 text-gray-500 border rounded shadow"
                     type="text"
                     name="value"
-                    style={{ flex: "10", padding : "5px"}}
                     placeholder = "해야 할 일을 입력하세요"
                     value={props.value}
                     onChange = {handleChange}
                     />
                 <input 
+                    className='p-2 text-blue-400 border-2 border-blue-400 rounded hover:text-white hover:bg-blue-200'
                     type = "submit"
                     value = "입력"
-                    className="btn"
-                    style = {{flex : "1"}}
                 />
             </form>
   )
